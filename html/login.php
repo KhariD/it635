612107a291e1b1d0ef22e981f5d3fa44db340448
Kharid <kld22@njit.edu>
2018-03-06 02:03:23 -0500

    fixes

--- html/login.php
+++ html/login.php
@@ -19,9 +19,11 @@ if ( $result->num_rows == 0 )
 }
 else { // User exists
     $user = $result->fetch_assoc();
+    echo "<br>user exists<br>"
 
     if ($_POST['password'] == $user['pass']) 
     {   
+
         $_SESSION['user'] = $user['user'];
         $_SESSION['fname'] = $user['fname'];
         $_SESSION['lname'] = $user['lname'];
