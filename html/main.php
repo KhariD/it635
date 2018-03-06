65e497e676685478ee7982c8958f8d140a1d19ad
Kharid <kld22@njit.edu>
2018-03-06 01:34:04 -0500

    hello

---
+++ html/main.php
@@ -0,0 +1,45 @@
+<?php
+/* Main page with two forms: sign up and log in */
+require 'db.php';
+session_start();
+?>
+
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+{
+    if (isset($_POST['login'])) 
+    { //user logging in
+        
+        require 'login.php';
+    
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+  <div class="form">
+    <div id="login">   
+        <h1>Welcome Back!</h1>
+        <form action="index.php" method="post" autocomplete="off">
+            <label>
+                Username<span class="req">*</span>
+            </label>
+            <input type="user" required autocomplete="off" name="user"/>
+            
+            <label>
+                Password<span class="req">*</span>
+            </label>
+            <input type="password" required autocomplete="off" name="password"/>
+          
+            <button class="button button-block" name="login" />Log In</button>
+        </form>
+    </div><!-- tab-content -->
+</div> <!-- /form -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+</body>
+</html>
