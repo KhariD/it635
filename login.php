a934e6d91202e40a58a8c233e91c606bebe51841
KhariD <31661446+KhariD@users.noreply.github.com>
2018-03-05 21:31:25 -0500

    adding php files

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+include 'usedCarDB.inc'
+
+$usr = $_POST['usr']
+$pass = $_POST['pass'];
+
+//Check if user in rep table
+
+
+//if so, validate login for rep
+
+//Else then they are in owner table
+
+//if in owner table validate login for owner
+
+
+
+
+
+
+
+?>
