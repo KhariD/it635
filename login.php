88ae6f77f845a7b649229f14f63d2861862936a2
Kharid <kld22@njit.edu>
2018-03-05 23:21:23 -0500

    whole lotta gang stuff

--- login.php
+++ login.php
@@ -1,17 +1,27 @@
 <?php
 include 'usedCarDB.inc'
 
-$usr = $_POST['usr']
+if (!isset($_POST))
+{
+	$msg = "NO POST MESSAGE SET";
+	echo json_encode($msg);
+	exit(0);
+}
+
+$usr = $_POST['usr'];
 $pass = $_POST['pass'];
 
-//Check if user in rep table
-
-
-//if so, validate login for rep
-
-//Else then they are in owner table
-
-//if in owner table validate login for owner
+if (validateRep($user, $pass))
+{
+    if (determineUser($user))
+    {
+        return "rep";
+    }
+    else
+    {
+        return "owner";
+    }
+}
 
 
 
