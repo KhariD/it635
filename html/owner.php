81652d44a59ec593ac408557979a73ae5e90a811
Kharid <kld22@njit.edu>
2018-03-19 00:11:37 -0400

    Add vehicle

--- html/owner.php
+++ html/owner.php
@@ -27,3 +27,29 @@ else {
     echo "Commission %: ".$com."<br>";
 }
 ?>
+
+<!DOCTYPE html>
+<html>
+<body>
+  <div class="add">
+    <div id="add">   
+        <form action="owner.php" method="post" autocomplete="off">
+            <br>
+            <label>
+                Add New Vehicle!
+            </label>
+            <br>
+            <input type="text" name="vin" placeholder="Enter vin"/><br>
+            <input type="text" name="make" placeholder="Enter make"/><br>
+            <input type="text" name="model" placeholder="Enter model"/><br>
+            <input type="text" name="year" placeholder="Enter year"/><br>
+            <input type="text" name="miles" placeholder="Enter milage"/><br>
+            <input type="text" name="type" placeholder="Enter type"/><br>
+            <input type="text" name="color" placeholder="Enter color"/><br>
+            <input type="text" name="trans" placeholder="Enter trans"/><br>
+            <input type="text" name="price" placeholder="Enter price"/><br>
+            <button class="button button-block" name="addVeh" />Add Vehicle</button>
+        </form>
+    </div><!-- tab-content -->
+</body>
+</html>
