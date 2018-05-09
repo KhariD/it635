29842d97860fd7c3f9773eb3bfa93e1fe6c358ba
Kharid <kld22@njit.edu>
2018-05-09 13:58:52 -0400

    backup

--- backup.sh
+++ backup.sh
@@ -2,14 +2,15 @@
 
 echo "### Mysql Backup ###"
 
-FILE=Used_Cars.sql.`date + "%Y%m%d"`
+FILE=Used_Cars_BACKUP.sql.`date + "%y%m%d"`
+DBSERVER=127.0.0.1
 DATABASE=Used_Cars
 USER=root
 PASS=monkey
 
-mysqldump --opt --protocol=TCP --user=${USER} --password=${PASS} ${DATABASE} > ${FILE}
+mysqldump --opt --protocol=TCP --user=${USER} --password=${PASS} --host=${DBSERVER} ${DATABASE} > ${FILE}
 
 gzip $FILE
 
-echo "${FILE}.gz was created: "
-ls -l ${FILE}.gz
+echo "${FILE} was created: "
+ls -l ${FILE}
